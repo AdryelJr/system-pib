21c5e707950f18dc2a205142c957b677a6b53a23
Adryel <adryeljuniods@gmail.com>
2024-02-01 12:15:17 -0300

    Update index.tsx

--- src/componentes/InfoDia/index.tsx
+++ src/componentes/InfoDia/index.tsx
@@ -1,14 +1,15 @@
-import { ptBR } from 'date-fns/locale';
-import { format } from 'date-fns';
-import './style.scss';
 import { Link } from 'react-router-dom';
+import './style.scss';
 
 export const InfoDia = ({ id, data, horario, tipoCulto }: any) => {
     const [dia, mes, ano] = data.split('/');
     const mesesAbreviados = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'];
-    const nomeDoMes = mesesAbreviados[parseInt(mes, 10) - 1];
+    const nomeDoMe = mesesAbreviados[parseInt(mes, 10) - 1];
     const dataObj = new Date(`${mes}-${dia}-${ano}`);
-    const nomeDia = capitalize(format(dataObj, 'EEEE', { locale: ptBR }));
+    const capitalizarPrimeiraLetra = (str: string) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+    const nomeDia = capitalizarPrimeiraLetra(dataObj.toLocaleDateString('pt-BR', { weekday: 'long' }));
 
     const informacoesDia = {
         data: data,
@@ -20,7 +21,7 @@ export const InfoDia = ({ id, data, horario, tipoCulto }: any) => {
         <Link to={{ pathname: `/detalhes/${id}`, state: { informacoesDia } } as any} className="content-infoDia">
             <div className="div-colum">
                 <span className="dia-culto">{dia}</span>
-                <span className="mes-culto">{nomeDoMes}</span>
+                <span className="mes-culto">{nomeDoMe}</span>
                 <span className="barrinha">|</span>
             </div>
             <div className="div-row-content">
@@ -34,8 +35,4 @@ export const InfoDia = ({ id, data, horario, tipoCulto }: any) => {
             </div>
         </Link>
     );
-};
-
-const capitalize = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
 };
