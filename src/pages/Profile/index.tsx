1909e030e8c7b22bd59a8d78055d2e306c8374ad
Adryel <adryeljuniods@gmail.com>
2024-02-05 18:25:04 -0300

    up code

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,36 +1,47 @@
-import { updateProfile, User } from "firebase/auth";
-import { auth } from "../../services/firebase";
+import { useNavigate } from 'react-router-dom';
+import './style.scss'
+import { useEffect, useState } from 'react';
+import LoadingSpinner from '../../componentes/loadingComponent';
+import { useUser } from '../../context/AuthContext';
+
 
 export function Profile() {
-    const newPhotoURL: string = 'https://pics.craiyon.com/2023-06-27/287f2a60c2e74386b5a89c517eb527dc.webp';
-    const newName: string = 'Adryel Santos';
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
 
-    function handleSubmit() {
-        const currentUser: User | null = auth.currentUser;
+    const { user } = useUser();
 
-        if (currentUser) {
-            updateProfile(currentUser, {
-                photoURL: newPhotoURL,
-                displayName: newName
-            })
-                .then(() => {
-                    console.log('Atualização do perfil bem-sucedida');
-                })
-                .catch((error: Error) => {
-                    console.error('Erro ao atualizar perfil:', error.message);
-                });
-        } else {
-            console.error('Usuário não autenticado');
-        }
+    const userPhotoURL = user?.photoURL ?? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGu0c9KGiQZE5Lofd9RW_1omNOVUSiZ3ZSIQ&usqp=CAU'
+    const userName = user?.displayName ?? 'Name'
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    }, []);
+
+    function handleHome() {
+        navigate('/home')
     }
 
     return (
-        <div>
-            <h1>Olá, perfil</h1>
+        <div className="container-profile">
+            {isLoading && <LoadingSpinner />}
 
-            <button onClick={handleSubmit}>
-                Clique
-            </button>
-        </div>
+            <div className='content-profile'>
+                <header>
+                    <svg onClick={handleHome} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M6 12L11 7M6 12L11 17" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+                </header>
+                <div className='div-bg'>
+                </div>
+                <main>
+                    <div className='div-img'>
+                        <img src={userPhotoURL} alt={userName} />
+                        <input type="file" placeholder='+' />
+                    </div>
+                    <h2>Adryel Júnio</h2>
+                </main>
+            </div >
+        </div >
     );
-}
+};
