e2f100a90fc9b993ed580169de4c9f8d207228b0
Adryel <adryeljuniods@gmail.com>
2024-02-08 10:01:52 -0300

    up scss

--- src/pages/Admin/CriarDia/index.tsx
+++ src/pages/Admin/CriarDia/index.tsx
@@ -3,8 +3,10 @@ import LogoImg from '../../../assets/logo-removebg-preview.png'
 import './style.scss'
 import { push, ref, set } from 'firebase/database';
 import { database } from '../../../services/firebase';
+import { useNavigate } from 'react-router-dom';
 
 export function CriarDia() {
+    const navigate = useNavigate();
     const [data, setData] = useState('');
     const [horario, setHorario] = useState('');
     const [tipoCulto, setTipoCulto] = useState('');
@@ -30,15 +32,22 @@ export function CriarDia() {
         alert('Dia adicionado com sucesso!')
     }
 
+    function handleVoltar() {
+        navigate('/home')
+    }
+
     return (
-        <div className='container-AdminAccount'>
+        <div className='container-criar-dia'>
+            <header>
+                <svg onClick={handleVoltar} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M6 12L11 7M6 12L11 17" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+                <img src={LogoImg} alt="logo-pib" />
+            </header>
             <div className='content'>
                 <div className='div-header'>
                     <div className='div-title'>
                         <h1>Louvor</h1>
                         <h2>PiB - Unaí</h2>
                     </div>
-                    <img src={LogoImg} alt="logo-pib" />
                 </div>
 
                 <div className='div-form'>
